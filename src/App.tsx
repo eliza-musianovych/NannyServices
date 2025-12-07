9497bd259e46e7e0717bc3b205a50431d935c158
Yelyzaveta Musianovych <eliza.musichka@gmail.com>
2025-12-07 15:21:15 +0100

    correct router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import css from './App.module.css';
 
 import clsx from 'clsx';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 import Header from './components/Header/Header';
@@ -35,13 +35,11 @@ function App() {
       />
 
       <main>
-        <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/nannies' element={<Nannies />} />
             <Route path='/favorites' element={<Favorites />}/>
           </Routes>
-        </BrowserRouter>
       </main>
 
       {modalMode && 
